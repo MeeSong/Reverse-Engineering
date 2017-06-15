44395d9663816c5073112dabad88f308c7abeba4
MeeSong <meesong@outlook.com>
2017-06-16 01:21:16 +0800

    process_access_mask macro2enum

--- Include/ntbase.h
+++ Include/ntbase.h
@@ -85,6 +85,46 @@ enum : BOOL
 
 //////////////////////////////////////////////////////////////////////////
 
+enum STANDARD_ACCESS_MASK : UINT32
+{
+    DELETE = 0x00010000L,
+    READ_CONTROL = 0x00020000L,
+    WRITE_DAC = 0x00040000L,
+    WRITE_OWNER = 0x00080000L,
+    SYNCHRONIZE = 0x00100000L,
+
+    STANDARD_RIGHTS_REQUIRED = 0x000F0000L,
+
+    STANDARD_RIGHTS_READ = READ_CONTROL,
+    STANDARD_RIGHTS_WRITE = READ_CONTROL,
+    STANDARD_RIGHTS_EXECUTE = READ_CONTROL,
+
+    STANDARD_RIGHTS_ALL = 0x001F0000L,
+
+    SPECIFIC_RIGHTS_ALL = 0x0000FFFFL,
+
+    //
+    // AccessSystemAcl access type
+    //
+
+    ACCESS_SYSTEM_SECURITY = 0x01000000L,
+
+    //
+    // MaximumAllowed access type
+    //
+
+    MAXIMUM_ALLOWED = 0x02000000L,
+
+    //
+    //  These are the generic rights.
+    //
+
+    GENERIC_READ = 0x80000000L,
+    GENERIC_WRITE = 0x40000000L,
+    GENERIC_EXECUTE = 0x20000000L,
+    GENERIC_ALL = 0x10000000L,
+};
+
 using LCID = UINT32;
 
 typedef struct _LUID {
